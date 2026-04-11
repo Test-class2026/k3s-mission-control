db60ffdb6eab8c909a2ce4bcca46b84600099e9b
Felix-OG <felix.omachonu@outsourceglobal.com>
2026-04-11 16:53:04 +0100

    fixing the bug

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const path    = require('path');
 const app     = express();
 
 // Add this line near the top of server.js — after the require statements
-if (process.env.APP_ENV === 'production') {
-  throw new Error("Deliberate crash for rollback demo!");
-}
 
 // Read config from environment variables (injected by ConfigMap)
 const PORT     = process.env.PORT     || 3000;
